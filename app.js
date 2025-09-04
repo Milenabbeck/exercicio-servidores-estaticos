82c80358e0ed4d9cd7c71def827dcdc20eafc099
Milena Beeck <milenasperia@gmail.com>
2025-09-04 11:47:12 -0300

    Modificando o método para receber um array de dados.

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 let button = document.getElementById('btnUsers');
+let div = document.getElementById('out');
 
 button.addEventListener('click', function () {
-    fetch('https://jsonplaceholder.typicode.com/posts/1')
-        .then((response) => response.json())
-        .then((json) => console.log(json));
-    //console.log("Clicou!");  
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        div.textContent = `Title: ${data[0].title}, Body: ${data[0].body}`
+        let listaOrdenada = "<ol>";
+
+        for (usuario in data) {
+            
+        }
+    })
 });
