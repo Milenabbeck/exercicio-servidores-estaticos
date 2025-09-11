6ca868c84d7bfdfb707fdb8cec61c2d1080886d2
Milena Beeck <milenasperia@gmail.com>
2025-09-11 11:25:19 -0300

    Adicionando tratamento de erro.

--- scripts/app.js
+++ scripts/app.js
@@ -2,15 +2,16 @@ let button = document.getElementById('btnUsers');
 let div = document.getElementById('out');
 
 button.addEventListener('click', function () {
-    fetch('https://jsonplaceholder.typicode.com/posts/99')
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (json) {
-        let post = Post.FromRaw(json);
-        div = post.renderFrom(div);
-        // console.log(post);
-        // div.innerText = json.body;
-        // div.innerText = Json.stringfy(json, null, 0);
-    })
+    fetch('https://jsonplaceholder.typicode.com/posts/101')
+        .then(function (response) {
+            if (response.ok) {
+                return response.json();
+            } else {
+                console.log("Erro");
+            }
+        })
+        .then(function (json) {
+            let post = Post.FromRaw(json);
+            div = post.renderFrom(div);
+        })
 });
