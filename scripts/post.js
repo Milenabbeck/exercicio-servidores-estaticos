a91ac092fcc3819a98818e89298fc76a872791c9
Milena Beeck <milenasperia@gmail.com>
2025-09-11 09:09:55 -0300

    Adicionando propriedades e construtor para a classe "Post".

---
+++ scripts/post.js
@@ -0,0 +1,9 @@
+// Data Model (Modelo de Dados)
+class Post {
+    constructor(userId, id, title, body) {
+        this.userId = userId;
+        this.id = id;
+        this.title = title;
+        this.body = body;
+    }
+}
