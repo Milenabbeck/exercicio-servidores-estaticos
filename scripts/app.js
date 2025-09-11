fc471b2b9c3858c16efd531e0ba55d88128609c5
Milena Beeck <milenasperia@gmail.com>
2025-09-11 11:45:45 -0300

    Colocando mensagem de erro com throw

--- scripts/app.js
+++ scripts/app.js
@@ -7,11 +7,25 @@ button.addEventListener('click', function () {
             if (response.ok) {
                 return response.json();
             } else {
-                console.log("Erro");
+                let errorMessage = "Recurso não encontrado ou servidor não disponível. Tente novamente mais tarde!"
+                throw new Error (errorMessage);
             }
         })
         .then(function (json) {
             let post = Post.FromRaw(json);
             div = post.renderFrom(div);
         })
+
+        .catch(function (error) {
+            renderError(error);
+        })
 });
+
+function renderError(error) {
+    div.innerHTML = "";
+
+    let h2 = document.createElement(`h2`);
+    h2.textContent = error;
+
+    div.appendChild(h2);
+}
